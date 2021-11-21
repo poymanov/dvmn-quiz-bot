22e0b534f24e8790682187c37a183bf34298f800
poymanov <n.poymanov@gmail.com>
2021-11-21 19:51:40 +0300

    Проверка ответа пользователя

--- src/tg_bot.py
+++ src/tg_bot.py
@@ -4,6 +4,7 @@ import os
 import logging
 import telegram
 import redis
+import random
 
 TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
 REDIS_URL = os.environ['REDIS_URL']
@@ -17,25 +18,63 @@ logger = logging.getLogger(__file__)
 redisConnection = redis.Redis(host=REDIS_URL, port=REDIS_PORT, db=REDIS_DB, password=REDIS_PASSWORD)
 
 
+def send_message(context, user_id, text, reply_markup=None):
+    context.bot.send_message(chat_id=user_id, text=text, reply_markup=reply_markup)
+
+
+def is_answer_correct(user_answer, answer):
+    return user_answer.casefold() == answer.casefold()
+
+
+def get_user_question(user_id):
+    question = redisConnection.get(user_id)
+
+    if question is None:
+        raise Exception('Ошибка получения вопроса для проверки ответа. Для следующего вопроса нажми "Новый вопрос"')
+
+    return question.decode('utf8')
+
+
+def get_answer(question):
+    questions_and_answers = get_questions_and_answers()
+
+    if question not in questions_and_answers:
+        raise Exception('Ошибка получения ответа на вопрос. Для следующего вопроса нажми "Новый вопрос"')
+
+    raw_answer = get_questions_and_answers()[question]
+    divider_index = raw_answer.index('.')
+    return raw_answer[:divider_index]
+
+
 def error(update, context):
     logger.exception('Telegram-бот упал с ошибкой')
 
 
 def menu_command(update, context):
     user_id = update.effective_chat.id
-    first_question = list(get_questions_and_answers())[0]
 
     if update.message.text == 'Новый вопрос':
-        context.bot.send_message(chat_id=user_id, text=first_question)
-        redisConnection.set(user_id, first_question)
-        print(redisConnection.get(user_id).decode('utf8'))
+        question = random.choice(list(get_questions_and_answers().keys()))
+        redisConnection.set(user_id, question)
+        context.bot.send_message(chat_id=user_id, text=question)
+    else:
+        try:
+            question = get_user_question(user_id)
+            answer = get_answer(question)
+
+            if is_answer_correct(update.message.text, answer):
+                return send_message(context, user_id,
+                                    'Правильно! Поздравляю! Для следующего вопроса нажми "Новый вопрос".')
+            else:
+                return send_message(context, user_id, 'Неправильно… Попробуешь ещё раз?')
+        except Exception as e:
+            send_message(context, user_id, str(e))
 
 
 def start_command(update, context):
     custom_keyboard = [['Новый вопрос', 'Сдаться'], ['Мой счёт']]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет! Я бот для викторин!",
-                             reply_markup=reply_markup)
+    send_message(context, update.effective_chat.id, "Привет! Я бот для викторин!", reply_markup)
 
 
 def main():
